0932cedee7979900f6247f277498aec7e00b0471
Killrahan <100357634+Killrahan@users.noreply.github.com>
2024-12-06 19:30:32 +0100

    Add files via upload

---
+++ human_activity.py
@@ -0,0 +1,199 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
+import toy_script 
+import os
+"""
+This file is the file where we built machine learning's method to predict the
+activity. 
+"""
+
+# The following function will be used to know which sensor shouldn't be considered.
+
+
+def number_missing():
+    """
+    Return the number and indexes (corresponding to a particular line) of time series 
+    that are either fully or partially missing for each sensors. 
+    Return value takes this shape : [[fully_missing2,partially_missing2,fully_missing_indexes2, partially_missing_indexes2]
+                                    ,...,[fully_missing32,partially_missing32,fully_missing_indexes32, partially_missing_indexes32]]
+
+    """
+    FEATURES = range(2, 33)
+    LS_path = os.path.join('./', 'LS')
+    missing_array = []
+    for f in FEATURES:
+        fully_index = 0
+        partially_index = 0
+        partially_missing = 0
+        fully_missing = 0
+        fully_indexes = []
+        partially_indexes = []
+        data = np.loadtxt(os.path.join(LS_path, 'LS_sensor_{}.txt'.format(f)))
+        for line in data:
+            missing_element = 0
+            for element in line:
+                if element == -999999.99:
+                    missing_element += 1
+            if missing_element == len(line):
+                fully_missing += 1
+                fully_indexes.append(fully_index)
+            elif missing_element != 0:
+                partially_missing += 1
+                partially_indexes.append(partially_index)
+            fully_index += 1
+            partially_index += 1
+
+        missing_array.append(
+            [fully_missing, partially_missing, fully_indexes, partially_indexes])
+
+    return missing_array
+
+
+def fill_missing(dataset, indexes, Average=True, KNN_imput=False):
+    """
+    Fill the missing values in a dataset.
+    args : 
+    dataset : the dataset to be modified. (1 txt file).
+    indexes : the indexes of line of time series that are either partially missing or
+              fully_missing. indexes[0] contains all time series that are fully missing
+              and indexes[1] contains all time series that are partially missing.
+    Average : If put to true use Average method to fill. 
+    KNN_imput : If put to true use KNN_imput to fill. 
+
+    return : a modified dataset.
+    """
+    if Average:
+        dataset_mean = []
+
+        for i in range(len(dataset)):
+            if not (-999999.99 in dataset[i]):
+                dataset_mean.append(np.average(dataset[i]))
+
+        dataset_mean = np.average(dataset_mean)
+        # print("Mean of the dataset : ", dataset_mean)
+
+        for i in range(len(dataset)):
+            # Deal with fully missing time series.
+            if i in indexes[0]:
+                for j in range(len(dataset[i])):
+                    dataset[i][j] = dataset_mean
+
+            elif i in indexes[1]:
+                """if (i == 944):
+                    print(f"index table : {indexes[1]} \n")
+                    print(f"dataset line : {dataset[i]} \n")"""
+                
+                non_missing = []
+
+                for j in range(len(dataset[i])):
+                    if dataset[i][j] != -999999.99:
+                        non_missing.append(dataset[i][j])
+
+                """if len(non_missing) < 1:
+                    print(f"empty non_missing for i = {i}\n")"""
+
+                average = np.average(non_missing)
+                # print(f"average = {average}")
+
+                for j in range(len(dataset[i])):
+                    if dataset[i][j] == -999999.99:
+                        dataset[i][j] = average
+
+    elif KNN_imput == True:
+        pass  # to implement later.
+
+    return dataset
+
+
+def build_dataset(useless_th, nb_series, nb_tot, Average=True, KNN_imput=False):
+    """
+    This function get rid of useless sensors, fill missing time series using either an
+    averaging method or KNN_imputation and build the sets X_train, y_train, X_validation
+    and y_validation that will be used to assess the model. 
+
+    Args : 
+    useless_th : number of missing series require to toss a sensor data. 
+    Average : If put to true use Average method to fill. 
+    KNN_imput : If put to true use KNN_imput to fill. 
+    nb_series : Number of time_series we want to put in the training set.
+    nb_tot : number of time series in the original dataset. 
+
+    return : [X_train, y_train, X_validation, y_validation]
+    """
+    if (Average == True and KNN_imput == True) or (Average == False and KNN_imput == False):
+        raise Exception("Choose one mode or the other.")
+
+    if not isinstance(useless_th, int):
+        raise Exception("Invalid Threshold.")
+
+    if nb_series > nb_tot:
+        raise Exception(
+            "Cannot pick more series than there is in the dataset.")
+
+    LS_path = os.path.join('./', 'LS')
+    TS_path = os.path.join('./', 'TS')
+    # Select the sensor we keep : (NB : for the moment only average method is built).
+
+    missing_array = number_missing()
+    # print(f"missing_array = {missing_array}")
+    f_indexes = []
+    f_index = 2
+    for i in missing_array:
+        if i[0] < useless_th:
+            f_indexes.append(f_index)
+        f_index += 1
+    f_index = 0
+    # print(f"f_indexes = {f_indexes}")
+
+    # Build sets and fill missing data :
+    X_train = np.zeros((nb_series, (len(f_indexes)*512)))
+    y_train = np.loadtxt(os.path.join(LS_path, 'activity_Id.txt'))[:nb_series]
+    X_validation = np.zeros((nb_tot - nb_series, (len(f_indexes)*512)))
+    y_validation = np.loadtxt(os.path.join(LS_path, 'activity_Id.txt'))[nb_series:]
+    X_test = np.zeros((nb_tot, (len(f_indexes) * 512)))
+        
+    data_array = []
+    for f in f_indexes:
+        data_curr = np.loadtxt(os.path.join(
+            LS_path, 'LS_sensor_{}.txt'.format(f)))
+
+        data_array.append(fill_missing(data_curr, [missing_array[f-2][2], missing_array[f-2][3]]))
+
+    index = 0
+    for f in f_indexes:
+        print(f"f = {f} \n")
+        X_train[:, (index)*512:(index+1)*512] = data_array[index][:nb_series]
+        X_validation[:, (index)*512:(index+1)*512] = data_array[index][nb_series:]
+        data = np.loadtxt(os.path.join(TS_path, 'TS_sensor_{}.txt'.format(f)))
+        X_test[:,(index)*512:(index+1)*512] = data
+        index += 1
+
+    print('X_train size: {}.'.format(X_train.shape))
+    print('y_train size: {}.'.format(y_train.shape))
+    print('X_validation size: {}.'.format(X_validation.shape))
+    print('y_validation size: {}.'.format(y_validation.shape))
+
+    return X_train, y_train, X_validation, y_validation, X_test
+
+
+if __name__ == '__main__':
+
+    print("Main file.")
+    LS_path = os.path.join('./', 'LS')
+    my_set = build_dataset(300, 3500, 3500)
+    X_train = my_set[0]
+    y_train = my_set[1]
+    X_validation = my_set[2]
+    y_validation = my_set[3]
+    X_test = my_set[4]
+    #test on K-neighbors : 
+    clf = KNeighborsClassifier(n_neighbors=5)
+    clf.fit(X_train, y_train)
+    y_predict = clf.predict(X_test)
+
+    toy_script.write_submission(y_predict)
+    
